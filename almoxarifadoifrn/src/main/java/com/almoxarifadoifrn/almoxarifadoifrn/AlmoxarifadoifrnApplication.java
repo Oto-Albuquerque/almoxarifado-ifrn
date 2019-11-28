ed7bd22f5c17bdbeddfa01bcc130528e5602fea2
otoal <otoal@10.104.1.180>
2019-11-28 15:24:03 -0300

    Inicio do almoxarifado virtual

---
+++ almoxarifadoifrn/src/main/java/com/almoxarifadoifrn/almoxarifadoifrn/AlmoxarifadoifrnApplication.java
@@ -0,0 +1,13 @@
+package com.almoxarifadoifrn.almoxarifadoifrn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmoxarifadoifrnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmoxarifadoifrnApplication.class, args);
+	}
+
+}
---
+++ almoxarifadoifrn/src/test/java/com/almoxarifadoifrn/almoxarifadoifrn/AlmoxarifadoifrnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almoxarifadoifrn.almoxarifadoifrn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmoxarifadoifrnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
